4443022da31fdf3ef06ee64f9e5f1c16481b599d
salissadiq <salissadiq@gmail.com>
2023-12-08 11:14:55 +0100

    Merge changes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vAoiquxcj4y5S49diz5LXToELDpPcPzuTRMaXP7Saw4=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6ikiwimYtcmYzcfTM0e1s9BuWAxlluJbJxQb4n+SKVw=\"\n}"
