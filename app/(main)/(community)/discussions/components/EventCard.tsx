ad8de61514d31e79dbdd9d947fa78399fec84a57
jawn <jawn@viableknowledgemasters.com>
2024-01-22 17:02:03 +0100

    finished mobile design for about page

--- app/(main)/(community)/discussions/components/EventCard.tsx
+++ app/(main)/(community)/discussions/components/EventCard.tsx
@@ -15,7 +15,7 @@ const EventCard: React.FC<{ event: EventProps }> = ({
 }) => {
 
   return (
-    <article className="w-full h-[4rem] md:h-[7rem] grid grid-cols-8 items-center my-1 cursor-pointer hover:bg-primary/10 drop-shadow-sm gap-4 md:gap-0 border-gray-500 transform transition-all ease-in-out hover:scale-[99%] duration-75 rounded-[0.5rem] p-1 md:p-2">
+    <article className="w-full font-sora h-[4rem] md:h-[7rem] grid grid-cols-8 items-center my-1 cursor-pointer hover:bg-primary/10 drop-shadow-sm gap-4 md:gap-0 border-gray-500 transform transition-all ease-in-out hover:scale-[99%] duration-75 rounded-[0.5rem] p-1 md:p-2">
       <Image
         src={
           event?.image || "../../../../public/images/group-of-girls.png"
@@ -29,7 +29,7 @@ const EventCard: React.FC<{ event: EventProps }> = ({
 
       <div className="col-span-6 flex flex-col items-start justify-start gap-1">
         <h4 className="text-sm font-semibold">{event?.title}</h4>
-        <p className="text-sm text-gray-300">{event.author}</p>
+        <p className="text-sm text-gray-300 font-quickSand">{event.author}</p>
         <p className="text-xs text-info">{event.createdAt} ({event.status})</p>
       </div>
 
