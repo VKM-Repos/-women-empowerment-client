22e631711080d3d7b8d5891014b7e0ef084568f0
jawn <jawn@viableknowledgemasters.com>
2024-02-23 11:33:27 +0100

    rename the continue button in the last step of creating org to submit et al

--- components/LandingPage/OrganizationCard.tsx
+++ components/LandingPage/OrganizationCard.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import LikeButton from "./LikeButton";
+import ShareDropdown from "./ShareDropDown";
 
 interface OrganizationProps extends Organization {
   otherProps?: string;
@@ -30,7 +31,7 @@ export const OrganizationCard: React.FC<{
       : text;
   };
   return (
-    <article className="border border-gray-500 shadow-sm bg-white self-stretch p-4 py-4 md:p-8 rounded-2xl max-md:max-w-full hover:bg-primary/10 transform transition-all ease-in-out duration-75">
+    <article className="border border-gray-500 shadow-sm bg-white self-stretch p-4 py-4 md:p-8 rounded-2xl max-md:max-w-full hover:bg-primary/10 transform transition-all ease-in-out duration-75 ">
       <div className="grid grid-cols-12 gap-5 place-content-center items-start ">
         <div className="col-span-8 md:col-span-10 h-full flex flex-col gap-2 items-start justify-between">
           <Link
@@ -55,35 +56,41 @@ export const OrganizationCard: React.FC<{
           <div className="justify-between border-gray-500  flex w-full gap-4 py-2 border-y items-center">
             <div className="items-center flex justify-start gap-4">
               <LikeButton organizationId={organization.id} />
-
-              {/* share button */}
-              <button className="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full">
-                <svg
-                  className="w-6 aspect-square"
-                  viewBox="0 0 25 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M8.35962 6L12.3596 2L16.3596 6"
-                    stroke="#787878"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M16.3596 10H18.3596C18.6248 10 18.8792 10.1054 19.0667 10.2929C19.2543 10.4804 19.3596 10.7348 19.3596 11V20C19.3596 20.2652 19.2543 20.5196 19.0667 20.7071C18.8792 20.8946 18.6248 21 18.3596 21H6.35962C6.0944 21 5.84005 20.8946 5.65251 20.7071C5.46498 20.5196 5.35962 20.2652 5.35962 20V11C5.35962 10.7348 5.46498 10.4804 5.65251 10.2929C5.84005 10.1054 6.0944 10 6.35962 10H8.35962"
-                    stroke="#787878"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M12.3596 2V15"
-                    stroke="#787878"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
+              <ShareDropdown>
+                <div className="w-full bg-primaryWhite py-4 p-2 flex flex-col items-start justify-start gap-4">
+                  <button className="w-full p-2 flex items-center justify-start gap-6 text-gray-300 hover:bg-primary/10 rounded">
+                    <svg
+                      className="w-6 aspect-square"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        {" "}
+                        <path
+                          d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"
+                          stroke="currentColor"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                        ></path>{" "}
+                        <path
+                          d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"
+                          stroke="currentColor"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                        ></path>{" "}
+                      </g>
+                    </svg>
+                    <span>copy link</span>
+                  </button>
+                </div>
+              </ShareDropdown>
             </div>
 
             <button className="flex flex-nowrap items-center gap-1.5 whitespace-nowrap">
@@ -113,7 +120,11 @@ export const OrganizationCard: React.FC<{
                 />
               </svg>
 
-              <Link href={organization.website} target="_blank" className="text-gray-300 font-sora text-center text-xs md:text-sm self-center hover:underline my-auto">
+              <Link
+                href={organization.website}
+                target="_blank"
+                className="text-gray-300 font-sora text-center text-xs md:text-sm self-center hover:underline my-auto"
+              >
                 visit website
               </Link>
             </button>
@@ -121,7 +132,10 @@ export const OrganizationCard: React.FC<{
         </div>
         <div className="flex flex-col col-span-4 md:col-span-2">
           <motion.img
-            src={organization.logo || "https://placehold.co/400x400?text=Women\n Hub"}
+            src={
+              organization.logo ||
+              "https://placehold.co/400x400?text=Women\n Hub"
+            }
             alt=""
             loading="lazy"
             className="w-full aspect-square justify-center items-center overflow-hidden rounded-md"
