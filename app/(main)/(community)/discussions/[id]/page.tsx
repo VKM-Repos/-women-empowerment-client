c70e0dae3bb34fe97d9fd9ca74731973e9b0fa28
jawnchuks <jawnchuks@gmail.com>
2024-07-12 10:20:01 +0100

    modified generate metadata function

--- app/(main)/(community)/discussions/[id]/page.tsx
+++ app/(main)/(community)/discussions/[id]/page.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import DiscussionInfo from '../components/DiscussionInfo';
 import { Discussion } from '@/lib/types/discussion.types';
+import { Metadata } from 'next';
 
-
-export default async function DiscussionDetailsPage({
+export default function DiscussionDetailsPage({
   params,
 }: {
   params: { id: string };
 }) {
   return <DiscussionInfo discussionId={params.id} />;
-  
 }
 
 const fetchDiscussionById = async (discussionId: string): Promise<Discussion> => {
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}discussions/${discussionId}`, {
-      cache: 'no-store'
+      cache: 'no-store',
     });
 
-    if (!response) {
+    if (!response.ok) {
       throw new Error('Failed to fetch discussion');
     }
 
@@ -30,40 +29,44 @@ const fetchDiscussionById = async (discussionId: string): Promise<Discussion> =>
   }
 };
 
-
 export async function generateMetadata({
   params,
 }: {
   params: { id: string };
-}) {
+}): Promise<Metadata> {
+  try {
+    const discussion = await fetchDiscussionById(params.id);
 
-  const discussion: any = fetchDiscussionById(params.id)
-
-  return {
-    title: discussion?.title,
-    description: discussion?.content,
-    openGraph: {
-      type: 'article',
-      locale: 'en_US',
-      url: `https://womenhub.org/discussions/${params.id}`,
-      title: discussion?.title,
-      description: discussion?.content,
-      images: [
-        {
-          url: discussion?.createdBy?.photoUrl,
-          width: 800,
-          height: 600,
-          alt: discussion?.createdBy?.name,
-        },
-      ],
-      siteName: 'Women Hub',
-    },
-    twitter: {
-      // card: 'summary_large_image',
-      // site: '@yourtwitterhandle',
-      title: discussion?.title,
-      description: discussion?.content,
-      images: discussion?.createdBy?.photoUrl,
-    },
-  };
+    return {
+      title: discussion?.title || 'Default Title',
+      description: discussion?.content || 'Default Description',
+      openGraph: {
+        type: 'article',
+        locale: 'en_US',
+        url: `https://womenhub.org/discussions/${params.id}`,
+        title: discussion?.title || 'Default Title',
+        description: discussion?.content || 'Default Description',
+        images: [
+          {
+            url: discussion?.createdBy?.photoUrl || '',
+            width: 800,
+            height: 600,
+            alt: discussion?.createdBy?.name || 'Default Alt Text',
+          },
+        ],
+        siteName: 'Women Hub',
+      },
+      twitter: {
+        title: discussion?.title || 'Default Title',
+        description: discussion?.content || 'Default Description',
+        images: discussion?.createdBy?.photoUrl || '',
+      },
+    };
+  } catch (error) {
+    console.error('Error generating metadata:', error);
+    return {
+      title: 'Error',
+      description: 'Error generating metadata',
+    };
+  }
 }
